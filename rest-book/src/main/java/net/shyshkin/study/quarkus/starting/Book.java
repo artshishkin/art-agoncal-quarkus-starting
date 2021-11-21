36fbbce03c1129526b6448255ad529401358a527
Art <d.art.shishkin@gmail.com>
2021-11-21 11:23:58 +0200

    18. Exposing the Book REST Endpoint (#2 Section 4: Developing the Application)

---
+++ rest-book/src/main/java/net/shyshkin/study/quarkus/starting/Book.java
@@ -0,0 +1,91 @@
+package net.shyshkin.study.quarkus.starting;
+
+import java.util.Objects;
+
+public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private int yearOfPublication;
+    private String genre;
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String author, int yearOfPublication, String genre) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id &&
+                yearOfPublication == book.yearOfPublication &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(genre, book.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, yearOfPublication, genre);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearOfPublication=" + yearOfPublication +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+}
--- rest-book/src/main/java/net/shyshkin/study/quarkus/starting/BookResource.java
+++ rest-book/src/main/java/net/shyshkin/study/quarkus/starting/BookResource.java
@@ -1,16 +1,53 @@
 package net.shyshkin.study.quarkus.starting;
 
+import com.github.javafaker.Faker;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Path("/api/books")
 public class BookResource {
 
+    private List<Book> books = IntStream.rangeClosed(1, 5)
+            .mapToObj(this::randomBook)
+            .collect(Collectors.toList());
+
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> allBooks() {
+        return books;
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Optional<Book> oneBook(@PathParam("id") int id) {
+        return books.stream()
+                .filter(book -> book.getId() == id)
+                .findAny();
+    }
+
+    @GET
+    @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello World!";
+    public int count() {
+        return books.size();
+    }
+
+    private Book randomBook(int id) {
+        return new Book(
+                id,
+                Faker.instance().book().title(),
+                Faker.instance().book().author(),
+                Faker.instance().date().birthday().getYear(),
+                Faker.instance().book().genre()
+        );
     }
 }
